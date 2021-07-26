516d7ca39dccb5fe29f2ac00ffe53fff47f07fa1
Alexander Okonkwo <ajokonkwo@icloud.com>
2021-07-25 20:53:00 -0700

    switched to snake case exlusively

--- main.py
+++ main.py
@@ -28,29 +28,29 @@ def check_oil_change():
     last_miles = int(last_miles)
     miles_till_oilchange = miles_between_changes - (miles - last_miles)
     if miles_till_oilchange == 0:
-        outputLabel.config(text="You are due for an oil change")
+        output_label.config(text="You are due for an oil change")
     if miles_till_oilchange > 0:
-        outputLabel.config(text="You have {} miles until next oil change.".format(miles_till_oilchange))
+        output_label.config(text="You have {} miles until next oil change.".format(miles_till_oilchange))
     if miles_till_oilchange < 0:
-        outputLabel.config(text="You are over due {} miles for an oil change.".format(abs(miles_till_oilchange)))
+        output_label.config(text="You are over due {} miles for an oil change.".format(abs(miles_till_oilchange)))
 
 #tab1 widgets
-lastmilesLabel = ttk.Label(tab1, text= "How many miles was your last oil change at?")
-lastmiles_entry = ttk.Entry(tab1, width=7)
+last_miles_label = ttk.Label(tab1, text= "How many miles was your last oil change at?")
+last_miles_entry = ttk.Entry(tab1, width=7)
 #tab1 positioning
-lastmilesLabel.grid(row=0, column=0)
-lastmiles_entry.grid(row=0, column=1)
+last_miles_label.grid(row=0, column=0)
+last_miles_entry.grid(row=0, column=1)
 
 #tab2 widgets
-milesLabel = ttk.Label(tab2, text= "Enter your cars current mileage:")
+miles_label = ttk.Label(tab2, text= "Enter your cars current mileage:")
 miles_entry = ttk.Entry(tab2, width=7)
-milesButton = ttk.Button(tab2, text="Enter", command=check_oil_change)
-outputLabel = ttk.Label(tab2, text="")
+miles_button = ttk.Button(tab2, text="Enter", command=check_oil_change)
+output_label = ttk.Label(tab2, text="")
 #tab2 positioning
-milesLabel.grid(row=1, column=0)
+miles_label.grid(row=1, column=0)
 miles_entry.grid(row=1, column=1)
-milesButton.grid(row=1, column=2)
-outputLabel.grid(row=2, column=1)
+miles_button.grid(row=1, column=2)
+output_label.grid(row=2, column=1)
 
 root.mainloop()
 
