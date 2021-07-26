168f4e38db6cc48f3fc06ed734768887b16c2dd2
Alexander Okonkwo <ajokonkwo@icloud.com>
2021-07-25 23:12:20 -0700

    added event handling functions

--- main.py
+++ main.py
@@ -34,6 +34,15 @@ def check_oil_change():
     if miles_till_oilchange < 0:
         output_label.config(text="You are over due {} miles for an oil change.".format(abs(miles_till_oilchange)))
 
+#tab1 event handling
+def UpdateMenu_SelectionEvent(value):
+    if value == 'Update Oil Change':
+        last_miles_label.config(text="How many miles was your last oil change at?")
+    if value == 'Update Tire Rotation':
+        last_miles_label.config(text="How many miles was your last tire rotation at?")
+    if value == 'Update Spark Plugs':
+        last_miles_label.config(text="How many miles was your spark plug change at?")
+
 #tab1 widgets
 maintenance_history_choices = [  #drop down menu options
     "Update Oil Change",
@@ -42,8 +51,8 @@ maintenance_history_choices = [  #drop down menu options
 ]
 clicked = StringVar()
 clicked.set( "Update" )
-maintenance_history_choices_dropdown = OptionMenu(tab1, clicked, *maintenance_history_choices)
-last_miles_label = ttk.Label(tab1, text= "How many miles was your last oil change at?")
+maintenance_history_choices_dropdown = OptionMenu(tab1, clicked, *maintenance_history_choices, command=UpdateMenu_SelectionEvent)
+last_miles_label = ttk.Label(tab1, text= "Choose maintenance to update")
 last_miles_entry = ttk.Entry(tab1, width=7)
 history_msg = StringVar()
 history_msg.set("Oil change: {} \nTire Rotation: {} \nSpark Plugs: {}".format("?","?","?"))
@@ -53,6 +62,17 @@ maintenance_history_choices_dropdown.grid(row=0, column=0, sticky="W")
 last_miles_label.grid(row=1, column=0)
 last_miles_entry.grid(row=1, column=1)
 maintenance_history_message.grid(row=2, column=0, sticky="W")
+#tab2 event handling
+def CheckMenu_SelectionEvent(value):
+    if value == 'Check Oil Change':
+        # miles_button['command']=test
+        miles_button.config(command=check_oil_change)
+    elif value == 'Check Tire Rotation':
+        miles_button.config(command=test)
+    elif value == 'Check Spark Plugs':
+        pass
+
+
 #tab2 widgets
 miles_label = ttk.Label(tab2, text= "Enter your cars current mileage:")
 miles_entry = ttk.Entry(tab2, width=7)
@@ -65,7 +85,7 @@ maintenance_choices = [  #drop down menu options
 ]
 clicked = StringVar()
 clicked.set( "Check" )
-maintenance_choices_dropdown = OptionMenu(tab2, clicked, *maintenance_choices)
+maintenance_choices_dropdown = OptionMenu(tab2, clicked, *maintenance_choices, command=CheckMenu_SelectionEvent)
 #tab2 positioning
 maintenance_choices_dropdown.grid(row=0, column=0, sticky="W")
 miles_label.grid(row=1, column=0)
